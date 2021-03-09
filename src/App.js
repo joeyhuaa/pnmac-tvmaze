046d90b199e045e026f42622bcca01bd8b2e953f
joeyhuaa <joeyhua17@gmail.com>
2021-03-08 22:32:43 -0800

    v2

--- src/App.js
+++ src/App.js
@@ -2,32 +2,22 @@ import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  let [shows, setShows] = useState([])
   let [query, setQuery] = useState('')
   let [searchResults, setSearchResults] = useState([])
+  let [justFetched, setJustFetched] = useState(false)
 
   useEffect(() => {
-    fetchApi()
-  }, [])
-
-  useEffect(() => {
-    console.log(searchResults)
+    if (query.length > 0) setJustFetched(true)
   }, [searchResults])
 
-  let fetchApi = async () => {
-    let response = await fetch('https://api.tvmaze.com/search/shows?q=walking')
-    let data = await response.json()
-    setShows(data)
-  }
+  useEffect(() => {
+    if (justFetched) setJustFetched(false)
+  }, [query])
 
-  let search = () => {
-    if (query.length > 0) {
-      // take query and find matches in shows (name of show)
-      // pull the shows the match the query
-      let matches = shows.filter(show => show.show.name.toLowerCase().includes(query.toLowerCase()))
-      // set them in state
-      setSearchResults(matches)
-    }
+  let fetchApi = async () => {
+    let response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+    let data = await response.json()
+    setSearchResults(data)
   }
 
   return (
@@ -44,20 +34,25 @@ function App() {
             id='text-input'
             onChange={e => setQuery(e.target.value)}
           />
-          <button onClick={search}>Search</button>
+          <button onClick={fetchApi}>Search</button>
         </div>
 
         <div id='search-results'>
-          {searchResults.map(s => (
-            // name, img, link (embed in name)
-            <div className='show-card'>
-              <img src={s.show.image.medium}></img>
-              <div style={{display:'block', marginLeft:'30px'}}>
-                <a href={s.show.url} target="_blank"><h4 className='show-title'>{s.show.name}</h4></a>
-                <div className='show-summary' dangerouslySetInnerHTML={{ __html: s.show.summary }} />
+          {searchResults.length > 0 &&
+            searchResults.map(s => (
+              // name, img, link (embed in name)
+              <div className='show-card'>
+                <img src={s.show.image ? s.show.image.medium : null} alt={s.show.name}></img>
+                <div style={{display:'block', marginLeft:'30px'}}>
+                  <a href={s.show.url} target="_blank"><h4 className='show-title'>{s.show.name}</h4></a>
+                  <div className='show-summary' dangerouslySetInnerHTML={{ __html: s.show.summary }} />
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          }
+          {searchResults.length === 0 && justFetched &&
+            <h4>No results found :/</h4>
+          } 
         </div>
       </header>
     </div>
